1b6c9764cb3931377bfa43a65034e537ec24f40e
Peter Otido <peterotido@gmail.com>
2023-10-31 02:00:49 -0700

    %T for type referenced in variable

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	remainingTickets = -1
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Println(userName)
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
